48da2051623b9dacb61a19818a811820f512f1c4
rennitbaby <work.kevin.ren@gmail.com>
2021-09-22 00:18:18 -0700

    added custom permissions, SuperuserOrAuthenticatedReadyOnly, look into POSTMAN, see if we can test with added cookies

--- sms/permissions.py
+++ sms/permissions.py
@@ -1,7 +1,29 @@
-from rest_framework import permissions
+from rest_framework import permissions, SAFE_METHODS
 
 # add custom permissions for the Student Management System
 # all users are able to GET
 # only super users can POST, PUT, PATCH, DELETE
 # there should really be only one superuser in one school
 # to prevent data coming in from multiple directions, its more safe to have other staff to paper record it, and then superuser enters data
+
+
+class IsSuperuserOrAuthenticatedReadOnly(permissions.BasePermission):
+    message = 'Sorry, you do not have sufficient permissions to do such actions.'
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.is_superuser:
+            return True
+
+        if request.user.is_authenticated and not request.user.is_superuser:
+            return request.method in SAFE_METHODS
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated and request.user.is_superuser:
+            return True
+
+        if request.user.is_authenticated and not request.user.is_superuser:
+            return request.method in SAFE_METHODS
+
+        return False
--- sms/views.py
+++ sms/views.py
@@ -1,31 +1,39 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 
+from .permissions import IsSuperuserOrAuthenticatedReadOnly
 from .models import School, Program, Rotation, Student
 from .serializers import SchoolSerializer, ProgramSerializer, RotationSerializer, StudentSerializer
 # Create your views here.
 
+# note allow any is only for dev purpose, remove it after front end is built and we can properly test
 
 # need to customize permissions for each view, only admins can have DEL, POST, PUT, PATCH priv
+
+
 class SchoolView(viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny |
+                          IsSuperuserOrAuthenticatedReadOnly]
 
 
 class ProgramView(viewsets.ModelViewSet):
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny |
+                          IsSuperuserOrAuthenticatedReadOnly]
 
 
 class RotationView(viewsets.ModelViewSet):
     queryset = Rotation.objects.all()
     serializer_class = RotationSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny |
+                          IsSuperuserOrAuthenticatedReadOnly]
 
 
 class StudentView(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny |
+                          IsSuperuserOrAuthenticatedReadOnly]
