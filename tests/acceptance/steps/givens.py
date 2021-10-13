5b93b6e86175835aa36c7efc3498ecaeab749a13
0x5929 <mail.kevin.ren@gmail.com>
2021-10-13 15:33:15 -0700

    addd onto given

--- tests/acceptance/steps/givens.py
+++ tests/acceptance/steps/givens.py
@@ -8,5 +8,14 @@ def logged_on_as_reg_user_is_office_true(context):
 
 
 @given('logged on as regular user with is_office set to false')
-def logged_on_as_reg_user_is_office_true(context):
+def logged_on_as_reg_user_is_office_false(context):
+    pass
+
+@given('logged on as staff office user')
+def logged_on_as_staff_user_is_office_true(context):
+    pass
+
+
+@given('logged on as staff user with is_office set to false')
+def logged_on_as_staff_user_is_office_false(context):
     pass
