eaa910ab50121c3e812d91a04d4923b5e4f02d9c
0x5929 <mail.kevin.ren@gmail.com>
2021-11-04 15:57:34 -0700

    splitted settings into dev, test and prod

---
+++ core/settings/dev.py
@@ -0,0 +1,56 @@
+from .common import *
+
+import environ
+
+env = environ.Env()
+
+# Take environment variables from .env file
+environ.Env.read_env('core/settings/.dev-env')
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+# dev database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': 'postgres',
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+    }
+}
+
+
+# email verification settings
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE = True
+LOGIN_URL = 'http://localhost:8000/auth/login'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+
+
+# Google Sheet Migration (SWITCH TO TRUE AFTER TESTING, AND IN PRODUCTION)
+MIGRATE_GOOGLE_SHEET = True
+
+# cors settings, ie for react apps
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
+
+ENV = 'DEV'
