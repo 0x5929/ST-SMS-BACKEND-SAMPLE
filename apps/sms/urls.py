1eab7d4f69b8caf55bfb576b3627ccbed71ce360
rennitbaby <work.kevin.ren@gmail.com>
2022-05-22 10:36:30 -0700

    added some notes on how to data dump, and how to request URLs for datadump and stats, and one migration? not sure what taht is

--- apps/sms/urls.py
+++ apps/sms/urls.py
@@ -14,7 +14,10 @@ router.register('students', StudentView, basename='students')
 
 
 urlpatterns = [
+    # usage: http://127.0.0.1:8000/api/sms/google_sheet_datadump/?ssid=<spreadsheet id>&sid=<sheet id>&school_name=<school name value, ie: STI>
     path('google_sheet_datadump/', GoogleSheetDataDumpView.as_view()),
+
+    # usage: http://127.0.0.1:8000/api/sms/student_statistics/
     path('student_statistics/', StudentStatisticsView.as_view()),
 ]
 
