80b137dc369a59a39b116d98752c75b8a5b9c9e0
0x5929 <mail.kevin.ren@gmail.com>
2022-10-14 12:36:12 -0700

    added GET route aka routing back to POST as its default for token/refresh/ this is what will be used in front ends as well

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -5,6 +5,7 @@ from dj_rest_auth.jwt_auth import get_refresh_view
 
 from rest_framework_simplejwt.views import TokenVerifyView
 
+from .views import RefreshTokenView
 
 # setting app_name and namespace in core/urls.py will cause a ReverseMatch Exception,
 # django_rest_auth source needs to be altered to add a reverse match app name like so: auth:account_confirmation_email in order to work
@@ -21,7 +22,7 @@ urlpatterns = [
      path('logout/', LogoutView.as_view()),
 
      path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-     path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
+     path('token/refresh/', RefreshTokenView.as_view(), name='token_refresh'),
      
      path('verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
      path('account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from dj_rest_auth.jwt_auth import get_refresh_view
 
 # Create your views here.
+class RefreshTokenView(get_refresh_view()):
+    def get(self, request):
+
+        # route to POST as it is default setforth by simple_jwt
+        return super().post(request)
